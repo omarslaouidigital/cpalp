468f33aa705a7200e897322e10c3645f6d72296d
lumustrip <102931717+omarslaouidigital@users.noreply.github.com>
2022-10-14 18:58:36 +0100

    analytics push

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
+import Head from 'next/head';
+import React from 'react';
 import '../styles/globals.css'
 import '../styles/loader.css'
+import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
-  	return <Component {...pageProps} />
+	return (
+		<>
+			<Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+			<Script strategy="lazyOnload">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
+
+
+			<Head>
+				<title>Welcome!</title>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 
